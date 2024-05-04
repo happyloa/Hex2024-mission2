9fab74fdb7c38437a1f8f9d25012632047cd6023
AaronHsieh <piafyoyo06@gmail.com>
2024-05-04 11:03:34 +0800

    引入價目表元件

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import styles from "./page.module.css";
 
 import ServiceTypes from "@/components/services/ServicesTypes";
 import ServiceProcess from "@/components/services/ServicesProcess";
+import ServicesPricing from "@/components/services/ServicesPricing";
 
 export default function Home() {
   return (
     <>
       <ServiceTypes />
       <ServiceProcess />
+      <ServicesPricing />
     </>
   );
 }

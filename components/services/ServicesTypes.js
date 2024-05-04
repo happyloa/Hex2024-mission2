3082c8edb941c21469cc3f2d7d824dc8a49d2455
AaronHsieh <piafyoyo06@gmail.com>
2024-05-04 08:56:30 +0800

    完成服務項目元件

--- components/services/ServicesTypes.js
+++ components/services/ServicesTypes.js
@@ -1,5 +1,38 @@
-import styles from "./ServicesTypes.module.cssService";
+import styles from "./ServicesTypes.module.css";
+
+import HeadingWithLine from "../ui/HeadingWithLine";
+
+const cardContent = [
+  {
+    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-visual.svg`,
+    cardTitle: "平面設計",
+  },
+  {
+    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-ui.svg`,
+    cardTitle: "UI 設計",
+  },
+  {
+    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-html%26css.svg`,
+    cardTitle: "切版服務",
+  },
+  {
+    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-front-end.svg`,
+    cardTitle: "前端開發",
+  },
+];
 
 export default function ServiceTypes() {
-  return <section className={styles.container}></section>;
+  return (
+    <section className={styles.container}>
+      <HeadingWithLine headingContent="服務項目" />
+      <div className={styles.card_container}>
+        {cardContent.map((val, idx) => (
+          <div className={styles.card} key={idx}>
+            <img src={val.cardIcon} className={styles.card_icon} />
+            <h3 className={styles.card_heading}>{val.cardTitle}</h3>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }

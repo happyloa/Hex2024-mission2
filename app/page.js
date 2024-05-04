e131f6eb45022ae878acc1ee5586322e9d51fc7d
AaronHsieh <piafyoyo06@gmail.com>
2024-05-04 09:36:19 +0800

    引入服務流程元件

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 import styles from "./page.module.css";
 
 import ServiceTypes from "@/components/services/ServicesTypes";
+import ServiceProcess from "@/components/services/ServicesProcess";
 
 export default function Home() {
-  return <ServiceTypes />;
+  return (
+    <>
+      <ServiceTypes />
+      <ServiceProcess />
+    </>
+  );
 }

5c7e1ddaa57b9a77cb70a5c3f98e345d288cbec4
AaronHsieh <piafyoyo06@gmail.com>
2024-05-04 21:58:15 +0800

    修改物件名稱，讓名稱更簡短

--- components/services/ServicesTypes.js
+++ components/services/ServicesTypes.js
@@ -4,20 +4,20 @@ import HeadingWithLine from "../ui/HeadingWithLine";
 
 const cardContent = [
   {
-    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-visual.svg`,
-    cardTitle: "平面設計",
+    Icon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-visual.svg`,
+    Title: "平面設計",
   },
   {
-    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-ui.svg`,
-    cardTitle: "UI 設計",
+    Icon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-ui.svg`,
+    Title: "UI 設計",
   },
   {
-    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-html%26css.svg`,
-    cardTitle: "切版服務",
+    Icon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-html%26css.svg`,
+    Title: "切版服務",
   },
   {
-    cardIcon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-front-end.svg`,
-    cardTitle: "前端開發",
+    Icon: `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-front-end.svg`,
+    Title: "前端開發",
   },
 ];
 
@@ -28,8 +28,8 @@ export default function ServiceTypes() {
       <div className={styles.card_container}>
         {cardContent.map((val, idx) => (
           <div className={styles.card} key={idx}>
-            <img src={val.cardIcon} className={styles.card_icon} />
-            <h3 className={styles.card_heading}>{val.cardTitle}</h3>
+            <img src={val.Icon} className={styles.card_icon} />
+            <h3 className={styles.card_heading}>{val.Title}</h3>
           </div>
         ))}
       </div>

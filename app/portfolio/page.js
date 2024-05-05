a92261b2beb0a4824de0707f12dfa2cd2976a246
AaronHsieh <piafyoyo06@gmail.com>
2024-05-06 00:44:56 +0800

    引入作品集元件

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,9 +1,11 @@
 import PortfolioQuote from "@/components/portfolio/PortfolioQuote";
+import PortfolioWorks from "@/components/portfolio/PortfolioWorks";
 
 export default function Portfolio() {
   return (
     <>
       <PortfolioQuote />
+      <PortfolioWorks />
     </>
   );
 }

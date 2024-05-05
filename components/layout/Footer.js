655c95af2ebe39b705e7bc5e51f7bf97f709c5df
AaronHsieh <piafyoyo06@gmail.com>
2024-05-05 08:25:15 +0800

    用 Link 包住圖片

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.logo_and_paragraph_and_email}>
-        <img
-          src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/logo.svg"
-          alt="網站 Logo"
-        />
+        <Link href="/">
+          <img
+            src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/logo.svg"
+            alt="網站 Logo"
+          />
+        </Link>
         <p>
           若有數位產品設計和開發相關問題
           <br />

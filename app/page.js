e63e27dd386d864e395745142ffc24e5127b835f
AaronHsieh <piafyoyo06@gmail.com>
2024-05-07 00:23:01 +0800

    引入首頁作品元件

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import HomeHero from "@/components/home/HomeHero";
+import HomeWorks from "@/components/home/HomeWorks";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <HomeWorks />
     </>
   );
 }

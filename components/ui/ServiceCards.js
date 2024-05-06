e600a880e1fe8f39689ce1b738ac29d29c8959f9
AaronHsieh <piafyoyo06@gmail.com>
2024-05-07 02:08:07 +0800

    將服務項目卡片拆分出來

---
+++ components/ui/ServiceCards.js
@@ -0,0 +1,13 @@
+import styles from "./ServiceCards.module.css";
+
+export default function ServiceCards({
+  Icon = `https://raw.githubusercontent.com/hexschool/2022-web-layout-training/d796f511bed88b5468b1c02b9dc1f17048b1448f/2024%20web-camp/service-item-visual.svg`,
+  Title = "服務項目",
+}) {
+  return (
+    <li className={styles.card}>
+      <img src={Icon} className={styles.card_icon} />
+      <h3 className={styles.card_heading}>{Title}</h3>
+    </li>
+  );
+}

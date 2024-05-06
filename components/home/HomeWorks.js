f5051fce533011399d1860996ace7841f5e6d608
AaronHsieh <piafyoyo06@gmail.com>
2024-05-07 01:47:39 +0800

    新增樣式、標題元件與語意標籤

--- components/home/HomeWorks.js
+++ components/home/HomeWorks.js
@@ -1,5 +1,14 @@
+import styles from "./HomeWorks.module.css";
+
+import HeadingWithLine from "../ui/HeadingWithLine";
+
 import StarTravel from "./works/StarTravel";
 
 export default function HomeWorks() {
-  return <StarTravel />;
+  return (
+    <main className={styles.container}>
+      <HeadingWithLine headingContent="作品介紹" />
+      <StarTravel />
+    </main>
+  );
 }

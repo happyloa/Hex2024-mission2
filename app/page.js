4e719d89b9f72bc95d16d807754dc95cc9307cf4
AaronHsieh <piafyoyo06@gmail.com>
2024-05-09 03:42:10 +0800

    引入首頁服務項目元件

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import HomeHero from "@/components/home/HomeHero";
 import HomeWorks from "@/components/home/HomeWorks";
+import HomeServiceTypes from "@/components/home/HomeServiceTypes";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
       <HomeWorks />
+      <HomeServiceTypes />
     </>
   );
 }

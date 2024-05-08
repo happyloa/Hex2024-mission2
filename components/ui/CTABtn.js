5182b5d305b1c181744729dae7b51280a1e44b87
AaronHsieh <piafyoyo06@gmail.com>
2024-05-08 08:54:17 +0800

    feat: 新增行動呼籲按鈕 ui 元件

---
+++ components/ui/CTABtn.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+import styles from "./CTABtn.module.css";
+
+export default function CTABtn({ href = "#", content = "輸入按鈕內容" }) {
+  return (
+    <Link href={href} className={styles.link}>
+      {content}
+      <img src="/arrow_forward.svg" />
+    </Link>
+  );
+}

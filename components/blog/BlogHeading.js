32fa58ad7020fcfd5cf258e30083120f475a0a8c
AaronHsieh <piafyoyo06@gmail.com>
2024-05-10 00:07:19 +0800

    新增部落格元件

---
+++ components/blog/BlogHeading.js
@@ -0,0 +1,12 @@
+import styles from "./BlogHeading.module.css";
+
+import HeadingWithLine from "../ui/HeadingWithLine";
+
+export default function BlogHeading() {
+  return (
+    <section className={styles.container}>
+      <HeadingWithLine headingContent="部落格" />
+      <p>不定期分享技術文章</p>
+    </section>
+  );
+}

951e8329c0cb2dda17285eba26fa428b9b4dc82f
AaronHsieh <piafyoyo06@gmail.com>
2024-05-09 23:18:36 +0800

    修改地圖連結

--- components/contact/ContactMap.js
+++ components/contact/ContactMap.js
@@ -4,7 +4,7 @@ export default function ContactMap() {
   return (
     <>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.127896098545!2d120.2975555759037!3d22.611698731534975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e037f2a5a9001%3A0x28af37aaf30894aa!2z6auY6ZuEODXlpKfmqJM!5e0!3m2!1szh-TW!2stw!4v1715231468967!5m2!1szh-TW!2stw"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d920.6737575148118!2d120.30963736960244!3d22.627860836586873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0491c81682db%3A0xca409b5dd8fb42e5!2zODAw6auY6ZuE5biC5paw6IiI5Y2A5rCR55Sf5LiA6LevNTbomZ8!5e0!3m2!1szh-TW!2stw!4v1715267865916!5m2!1szh-TW!2stw"
         className={styles.map}
         allowfullscreen="false"
         loading="lazy"

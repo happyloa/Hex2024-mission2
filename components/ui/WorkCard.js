2b1e98e6259531dae320189df48df524c7c4aaef
AaronHsieh <piafyoyo06@gmail.com>
2024-05-09 19:33:29 +0800

    引入 WorkModal 元件並使用 Event Prop 來呼叫 Modal

--- components/ui/WorkCard.js
+++ components/ui/WorkCard.js
@@ -1,4 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 import styles from "./WorkCard.module.css";
+
+import WorkModal from "./WorkModal";
 
 export default function WorkCard({
   imgSrc = `/image/work/work-image1.webp`,
@@ -6,20 +12,29 @@ export default function WorkCard({
   description = "請輸入作品描述",
   tags = ["作品標籤"],
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <article className={styles.card}>
-      <div className={styles.img_wrapper}>
-        <img src={imgSrc} alt={title} />
-      </div>
-      <div className={`${styles["card_content"]} ${styles["px-16"]}`}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-      <ul className={`${styles["card_tags"]} ${styles["px-16"]}`}>
-        {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
-    </article>
+    <>
+      <article className={styles.card} onClick={toggleModal}>
+        <div className={styles.img_wrapper}>
+          <img src={imgSrc} alt={title} />
+        </div>
+        <div className={`${styles["card_content"]} ${styles["px-16"]}`}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <ul className={`${styles["card_tags"]} ${styles["px-16"]}`}>
+          {tags.map((tag, index) => (
+            <li key={index}>{tag}</li>
+          ))}
+        </ul>
+      </article>
+      <WorkModal isOpen={isOpen} toggleModal={toggleModal} />
+    </>
   );
 }

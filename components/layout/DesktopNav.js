872de0d71adbb2ba15d86df46fbb464dadb7935b
AaronHsieh <piafyoyo06@gmail.com>
2024-05-10 13:00:54 +0800

    修改選單項目 hover 時的框框，變成用純 css

--- components/layout/DesktopNav.js
+++ components/layout/DesktopNav.js
@@ -1,91 +1,55 @@
-"use client";
-
 import Link from "next/link";
-import { useEffect } from "react";
 
 import styles from "./DesktopNav.module.css";
 
 export default function DesktopNav() {
-  useEffect(() => {
-    // 選取所有連結
-    const links = document.querySelectorAll(`nav ul li a`);
-
-    links.forEach((link) => {
-      // 為所有連結加上 mouseenter 監聽器
-      link.addEventListener("mouseenter", function () {
-        const boxes = link.parentNode.querySelectorAll("div");
-        boxes.forEach((box) => {
-          box.classList.add(styles.boxShow);
-        });
-      });
-
-      // 為所有連結加上 mouseleave 監聽器
-      link.addEventListener("mouseleave", function () {
-        const boxes = link.parentNode.querySelectorAll("div");
-        boxes.forEach((box) => {
-          box.classList.remove(styles.boxShow);
-        });
-      });
-    });
-  }, []);
-
   return (
     <nav className={styles.desktop_nav}>
       <ul className={styles.lists_wrapper}>
-        <li className={styles.nav_list}>
-          <Link href="/" className={styles.nav_link}>
-            首頁
-            <div className={`${styles["box"]} ${styles["boxUpperLeft"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxUpperRight"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxLowerRight"]}`}></div>
-            <div className={`${styles["box"]} ${styles["boxLowerLeft"]}`}></div>
-          </Link>
+        <li>
+          <Link href="/">首頁</Link>
+          <span className={`${styles["border"]} ${styles["top-left"]}`}></span>
+          <span className={`${styles["border"]} ${styles["top-right"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
         </li>
-        <li className={styles.nav_list}>
-          <Link href="/portfolio" className={styles.nav_link}>
-            作品集
-            <div className={`${styles["box"]} ${styles["boxUpperLeft"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxUpperRight"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxLowerRight"]}`}></div>
-            <div className={`${styles["box"]} ${styles["boxLowerLeft"]}`}></div>
-          </Link>
+        <li>
+          <Link href="/portfolio">作品集</Link>
+          <span className={`${styles["border"]} ${styles["top-left"]}`}></span>
+          <span className={`${styles["border"]} ${styles["top-right"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
         </li>
-        <li className={styles.nav_list}>
-          <Link href="/services" className={styles.nav_link}>
-            服務項目
-            <div className={`${styles["box"]} ${styles["boxUpperLeft"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxUpperRight"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxLowerRight"]}`}></div>
-            <div className={`${styles["box"]} ${styles["boxLowerLeft"]}`}></div>
-          </Link>
+        <li>
+          <Link href="/services">服務項目</Link>
+          <span className={`${styles["border"]} ${styles["top-left"]}`}></span>
+          <span className={`${styles["border"]} ${styles["top-right"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
         </li>
-        <li className={styles.nav_list}>
-          <Link href="/blog" className={styles.nav_link}>
-            部落格
-            <div className={`${styles["box"]} ${styles["boxUpperLeft"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxUpperRight"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxLowerRight"]}`}></div>
-            <div className={`${styles["box"]} ${styles["boxLowerLeft"]}`}></div>
-          </Link>
+        <li>
+          <Link href="/blog">部落格</Link>
+          <span className={`${styles["border"]} ${styles["top-left"]}`}></span>
+          <span className={`${styles["border"]} ${styles["top-right"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
         </li>
-        <li className={styles.nav_list}>
-          <Link href="/contact" className={styles.nav_link}>
-            聯絡我
-            <div className={`${styles["box"]} ${styles["boxUpperLeft"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxUpperRight"]}`}></div>
-            <div
-              className={`${styles["box"]} ${styles["boxLowerRight"]}`}></div>
-            <div className={`${styles["box"]} ${styles["boxLowerLeft"]}`}></div>
-          </Link>
+        <li>
+          <Link href="/contact">聯絡我</Link>
+          <span className={`${styles["border"]} ${styles["top-left"]}`}></span>
+          <span className={`${styles["border"]} ${styles["top-right"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+          <span
+            className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
         </li>
       </ul>
     </nav>

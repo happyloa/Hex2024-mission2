0172f49b07b31ec1d7363cad353f4957f8a613f8
AaronHsieh <piafyoyo06@gmail.com>
2024-05-10 11:44:52 +0800

    引入手機版文章分類選單元建

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,5 +1,11 @@
 import BlogHeading from "@/components/blog/BlogHeading";
+import BlogMobileNav from "@/components/blog/BlogMobileNav";
 
 export default function Blog() {
-  return <BlogHeading />;
+  return (
+    <>
+      <BlogHeading />
+      <BlogMobileNav />
+    </>
+  );
 }

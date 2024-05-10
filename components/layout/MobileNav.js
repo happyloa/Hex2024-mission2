cf9620c4d4cdacab6f1946253e3f94fdf2099ac8
AaronHsieh <piafyoyo06@gmail.com>
2024-05-10 12:59:52 +0800

    新增選單項目 hover 時的框框功能

--- components/layout/MobileNav.js
+++ components/layout/MobileNav.js
@@ -42,21 +42,61 @@ export default function MobileNav() {
           },
         }}>
         <nav>
-          <ul className={styles.nav_list}>
+          <ul className={styles.lists_wrapper}>
             <li>
               <Link href="/">首頁</Link>
+              <span
+                className={`${styles["border"]} ${styles["top-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["top-right"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
             </li>
             <li>
               <Link href="/portfolio">作品集</Link>
+              <span
+                className={`${styles["border"]} ${styles["top-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["top-right"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
             </li>
             <li>
               <Link href="/services">服務項目</Link>
+              <span
+                className={`${styles["border"]} ${styles["top-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["top-right"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
             </li>
             <li>
               <Link href="/blog">部落格</Link>
+              <span
+                className={`${styles["border"]} ${styles["top-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["top-right"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
             </li>
             <li>
               <Link href="/contact">聯絡我</Link>
+              <span
+                className={`${styles["border"]} ${styles["top-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["top-right"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-left"]}`}></span>
+              <span
+                className={`${styles["border"]} ${styles["bottom-right"]}`}></span>
             </li>
           </ul>
         </nav>

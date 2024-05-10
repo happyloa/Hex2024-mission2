27b9ab347d81df2ad4c2352f4ea91a5fc9ca74e5
AaronHsieh <piafyoyo06@gmail.com>
2024-05-10 12:09:44 +0800

    feat: 更新聯絡我頁面，讓它變得更好看

--- components/contact/ContactInfo.js
+++ components/contact/ContactInfo.js
@@ -9,14 +9,16 @@ export default function ContactInfo() {
       <div className={styles.info_and_form_wrapper}>
         <div className={styles.info}>
           <h3>讓我們建立聯繫</h3>
-          <a href="mailto:service@hexschool.com" className={styles.email}>
+          <a
+            href="mailto:service@hexschool.com?subject=您好，我想與您合作。?body=如果您看到這封 Email，請回覆我，期待與您合作，謝謝！"
+            className={styles.email}>
             Email：service@hexschool.com
           </a>
           <a href="tel:0911111111" className={styles.tel}>
             電話：0911111111
           </a>
           <p>
-            歡迎隨時透過表單、Email 或電話來連繫我！
+            歡迎隨時透過表單、Email 或電話來聯絡我！
             <br />
             期待能夠與您一同完成一份獨一無二且精緻的作品
           </p>
@@ -43,7 +45,7 @@ export default function ContactInfo() {
               <textarea
                 name="message"
                 placeholder="您想傳給我的訊息"
-                rows={10}></textarea>
+                rows={8}></textarea>
             </div>
             <div className={styles.submit}>
               <button type="submit">填好送出</button>

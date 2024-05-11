5ee20330d9831e0d31f76dcd675b5786d8087978
AaronHsieh <piafyoyo06@gmail.com>
2024-05-11 18:28:27 +0800

    引入部落格內容元件

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,11 +1,13 @@
 import BlogHeading from "@/components/blog/BlogHeading";
 import BlogMobileNav from "@/components/blog/BlogMobileNav";
+import BlogPosts from "@/components/blog/BlogPost";
 
 export default function Blog() {
   return (
     <>
       <BlogHeading />
       <BlogMobileNav />
+      <BlogPosts />
     </>
   );
 }

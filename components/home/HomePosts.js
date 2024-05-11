04ecddbfa31bb319b9a248ae13cd4f6adfb5a391
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 07:50:24 +0800

    引入文章卡片元件

--- components/home/HomePosts.js
+++ components/home/HomePosts.js
@@ -1,11 +1,13 @@
 import styles from "./HomePosts.module.css";
 
 import HeadingWithLine from "../ui/HeadingWithLine";
+import PostSliderCard from "../ui/PostSliderCard";
 
 export default function HomePosts() {
   return (
     <section className={styles.container}>
       <HeadingWithLine headingContent="精選文章" />
+      <PostSliderCard />
     </section>
   );
 }

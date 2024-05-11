8fa65b905a0981a76e5d2ff5731c5eeb397a99ae
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 06:18:18 +0800

    加入手機版分類選單

--- app/blog/[singlePost]/page.js
+++ app/blog/[singlePost]/page.js
@@ -1,15 +1,19 @@
 import styles from "./SinglePostPage.module.css";
 
 import SinglePost from "@/components/blog/Posts/SinglePost";
+import BlogMobileNav from "@/components/blog/BlogMobileNav";
 import BlogSideBar from "@/components/blog/BlogSideBar";
 
 export default function SinglePostPage() {
   return (
-    <section className={styles.container}>
-      <article>
-        <SinglePost />
-      </article>
-      <BlogSideBar />
-    </section>
+    <>
+      <BlogMobileNav />
+      <section className={styles.container}>
+        <article>
+          <SinglePost />
+        </article>
+        <BlogSideBar />
+      </section>
+    </>
   );
 }

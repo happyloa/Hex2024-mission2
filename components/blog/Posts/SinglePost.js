791075e57c3f8ff3bbf074097db900c29f0f3a6b
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 06:12:05 +0800

    新增單篇文章元件

---
+++ components/blog/Posts/SinglePost.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import styles from "./SinglePost.module.css";
+
+import posts from "@/lib/posts.json";
+
+export default function SinglePost() {
+  const pathname = usePathname();
+
+  const singlePost = pathname.split("/").pop();
+
+  const post = posts.find((post) => post.postLink === `/${singlePost}`);
+
+  // 如果找不到文章就顯示錯誤訊息
+  if (!post) {
+    return <p>沒有該篇文章</p>;
+  }
+
+  // 單篇文章內容
+  return (
+    <>
+      <section className={styles.meta_wrapper}>
+        <div className={styles.breadcrumb}>
+          <span>{post.postMeta.categories.join(", ")}</span>
+          <time>{post.postMeta.date + "發佈"}</time>
+        </div>
+        <h1>{post.postMeta.title}</h1>
+        <h2 className={styles.subtitle}>- 探索虛擬實境技術的現狀與前景 -</h2>
+      </section>
+      <section className={styles.content_wrapper}>
+        <p>{post.postMeta.summary}</p>
+        {/* 使用 map 方法來遍歷 postContent 裡的每個元素 */}
+        {post.postContent.map((content, index) => (
+          <div key={index}>
+            <h2>{content.title}</h2>
+            <p>{content.content}</p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}

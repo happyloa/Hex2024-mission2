89432833854014e9b0e7bad08bf96cd99fd6b55c
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 13:06:09 +0800

    引入手機版精選文章元件

--- components/home/HomePosts.js
+++ components/home/HomePosts.js
@@ -2,6 +2,7 @@ import styles from "./HomePosts.module.css";
 
 import HeadingWithLine from "../ui/HeadingWithLine";
 import PostSlider from "./posts/PostSlider";
+import MobilePosts from "./posts/MobilePosts";
 import CTABtn from "../ui/CTABtn";
 
 export default function HomePosts() {
@@ -9,6 +10,7 @@ export default function HomePosts() {
     <section className={styles.container}>
       <HeadingWithLine headingContent="精選文章" />
       <PostSlider />
+      <MobilePosts />
       <CTABtn href="/blog" content="更多文章" />
     </section>
   );

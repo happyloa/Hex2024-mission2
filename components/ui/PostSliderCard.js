55cdab9c0e51fa5c58594ddd2d7f398c6ce7d797
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 12:43:41 +0800

    移除 swiper-slide 元件

--- components/ui/PostSliderCard.js
+++ components/ui/PostSliderCard.js
@@ -4,25 +4,23 @@ import Link from "next/link";
 
 export default function PostSliderCard({ post }) {
   return (
-    <swiper-slide>
-      <Link href={"/blog" + post.postLink}>
-        <article>
-          <div className={styles.img_wrapper}>
-            <img src={post.postMeta.postThumb} alt={post.postMeta.title} />
+    <Link href={"/blog" + post.postLink}>
+      <article>
+        <div className={styles.img_wrapper}>
+          <img src={post.postMeta.postThumb} alt={post.postMeta.title} />
+        </div>
+        <div className={styles.content_wrapper}>
+          <h3>{post.postMeta.title}</h3>
+          <div className={styles.time_and_category}>
+            <ul>
+              {post.postMeta.categories.map((category, catIdx) => (
+                <li key={catIdx}>{category}</li>
+              ))}
+            </ul>
+            <time>{post.postMeta.date}</time>
           </div>
-          <div className={styles.content_wrapper}>
-            <h3>{post.postMeta.title}</h3>
-            <div className={styles.time_and_category}>
-              <ul>
-                {post.postMeta.categories.map((category, catIdx) => (
-                  <li key={catIdx}>{category}</li>
-                ))}
-              </ul>
-              <time>{post.postMeta.date}</time>
-            </div>
-          </div>
-        </article>
-      </Link>
-    </swiper-slide>
+        </div>
+      </article>
+    </Link>
   );
 }

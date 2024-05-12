fea24e215877eb86aaba216411230316795e9843
AaronHsieh <piafyoyo06@gmail.com>
2024-05-12 10:22:04 +0800

    引入精選文章單個卡片元件

--- components/blog/Posts/PostSlider.js
+++ components/blog/Posts/PostSlider.js
@@ -2,50 +2,21 @@
 
 import styles from "./PostSlider.module.css";
 
-import Link from "next/link";
 import { useRef } from "react";
 import { register } from "swiper/element/bundle";
+import swiperConfig from "@/swiperConfig";
 
 register();
 
-import posts from "@/lib/posts.json";
+import PostSliderCard from "@/components/ui/PostSliderCard";
 
 export default function PostSlider() {
   const swiperElRef = useRef(null);
 
   return (
     <>
-      <swiper-container
-        ref={swiperElRef}
-        autoplay="true"
-        slidesPerView="3"
-        loop="true"
-        pagination="true">
-        {posts.map((post, idx) => (
-          <swiper-slide key={idx}>
-            <Link href={"/blog" + post.postLink}>
-              <article>
-                <div className={styles.img_wrapper}>
-                  <img
-                    src={post.postMeta.postThumb}
-                    alt={post.postMeta.title}
-                  />
-                </div>
-                <div className={styles.content_wrapper}>
-                  <h3>{post.postMeta.title}</h3>
-                  <div className={styles.time_and_category}>
-                    <ul>
-                      {post.postMeta.categories.map((category, catIdx) => (
-                        <li key={catIdx}>{category}</li>
-                      ))}
-                    </ul>
-                    <time>{post.postMeta.date}</time>
-                  </div>
-                </div>
-              </article>
-            </Link>
-          </swiper-slide>
-        ))}
+      <swiper-container ref={swiperElRef} {...swiperConfig}>
+        <PostSliderCard />
       </swiper-container>
     </>
   );

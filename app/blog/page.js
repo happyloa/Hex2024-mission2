92dd07f8e16efdaa20ef7af3a822eca0b46ff3c2
AaronHsieh <piafyoyo06@gmail.com>
2024-05-15 11:19:46 +0800

    將 /blog 頁面包裹在 suspense 元件內

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,15 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
+
 import BlogHeading from "@/components/blog/BlogHeading";
 import BlogMobileNav from "@/components/blog/BlogMobileNav";
 import BlogPosts from "@/components/blog/BlogPost";
 
 export default function Blog() {
   return (
-    <>
+    <Suspense>
       <BlogHeading />
       <BlogMobileNav />
       <BlogPosts />
-    </>
+    </Suspense>
   );
 }

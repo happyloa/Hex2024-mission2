0719a94a4bfae5c45bb6b62174442df63b7a975a
AaronHsieh <piafyoyo06@gmail.com>
2024-05-15 11:30:18 +0800

    develop: 當點擊分類時為該分類增加 active class

--- components/blog/BlogSideBar.js
+++ components/blog/BlogSideBar.js
@@ -1,11 +1,17 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import styles from "./BlogSideBar.module.css";
 
-import Link from "next/link";
-
 export default function BlogSideBar() {
   const router = useRouter();
+  const [activeCategory, setActiveCategory] = useState("全部文章");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get("category") || "全部文章";
+    setActiveCategory(category);
+  }, []);
 
   const handleCategoryClick = (category) => {
     if (category === "全部文章") {
@@ -19,13 +25,31 @@ export default function BlogSideBar() {
     <aside className={styles.categories_container}>
       <nav className={styles.nav}>
         <ul>
-          <li onClick={() => handleCategoryClick("全部文章")}>全部文章</li>
-          <li onClick={() => handleCategoryClick("UI/UX 新知")}>UI/UX 新知</li>
-          <li onClick={() => handleCategoryClick("數位產品設計")}>
+          <li
+            className={activeCategory === "全部文章" ? styles.active : ""}
+            onClick={() => handleCategoryClick("全部文章")}>
+            全部文章
+          </li>
+          <li
+            className={activeCategory === "UI/UX 新知" ? styles.active : ""}
+            onClick={() => handleCategoryClick("UI/UX 新知")}>
+            UI/UX 新知
+          </li>
+          <li
+            className={activeCategory === "數位產品設計" ? styles.active : ""}
+            onClick={() => handleCategoryClick("數位產品設計")}>
             數位產品設計
           </li>
-          <li onClick={() => handleCategoryClick("平面設計")}>平面設計</li>
-          <li onClick={() => handleCategoryClick("前端開發")}>前端開發</li>
+          <li
+            className={activeCategory === "平面設計" ? styles.active : ""}
+            onClick={() => handleCategoryClick("平面設計")}>
+            平面設計
+          </li>
+          <li
+            className={activeCategory === "前端開發" ? styles.active : ""}
+            onClick={() => handleCategoryClick("前端開發")}>
+            前端開發
+          </li>
         </ul>
       </nav>
     </aside>

3b7ae3deb452b5ca4b93da0a7c37561b935c7ae8
AaronHsieh <piafyoyo06@gmail.com>
2024-05-17 07:58:13 +0800

    助教修改建議：文章列表也可改用 ul > li 結構

--- components/blog/Posts/PostList.js
+++ components/blog/Posts/PostList.js
@@ -13,28 +13,30 @@ export default function PostList({ activeCategory }) {
         );
 
   return (
-    <>
+    <ul className={styles.post_list_wrapper}>
       {filteredPosts.map((post, idx) => (
-        <Link href={"/blog" + post.postLink} key={idx}>
-          <article className={styles.post_list}>
-            <div className={styles.img_wrapper}>
-              <img src={post.postMeta.postThumb} alt={post.postMeta.title} />
-            </div>
-            <div className={styles.content_wrapper}>
-              <div className={styles.time_and_category}>
-                <time>{post.postMeta.date}</time>
-                <ul>
-                  {post.postMeta.categories.map((category, catIdx) => (
-                    <li key={catIdx}>{category}</li>
-                  ))}
-                </ul>
+        <li key={idx}>
+          <Link href={"/blog" + post.postLink}>
+            <article className={styles.post_list_card}>
+              <div className={styles.img_wrapper}>
+                <img src={post.postMeta.postThumb} alt={post.postMeta.title} />
               </div>
-              <h3>{post.postMeta.title}</h3>
-              <p>{post.postMeta.summary}</p>
-            </div>
-          </article>
-        </Link>
+              <div className={styles.content_wrapper}>
+                <div className={styles.time_and_category}>
+                  <time>{post.postMeta.date}</time>
+                  <ul>
+                    {post.postMeta.categories.map((category, catIdx) => (
+                      <li key={catIdx}>{category}</li>
+                    ))}
+                  </ul>
+                </div>
+                <h3>{post.postMeta.title}</h3>
+                <p>{post.postMeta.summary}</p>
+              </div>
+            </article>
+          </Link>
+        </li>
       ))}
-    </>
+    </ul>
   );
 }

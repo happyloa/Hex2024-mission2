a4e66f8372ab4a1655fc73f8b8a8f666fd9bad3c
AaronHsieh <piafyoyo06@gmail.com>
2024-05-17 17:24:22 +0800

    feat: 新增 isH1 props，讓有該 props 的元件變為 h1 標籤

--- components/ui/HeadingWithLine.js
+++ components/ui/HeadingWithLine.js
@@ -3,18 +3,22 @@ import styles from "./HeadingWithLine.module.css";
 export default function HeadingWithLine({
   headingContent = "輸入標題",
   light = false,
+  isH1 = false,
 }) {
   // 根據 light 屬性決定文字顏色
   const headingStyle = {
     color: light ? "var(--white)" : "var(--primary700)", // 如果 light 為 true 則文字顏色為白色，否則為原始顏色
   };
 
+  // 根據 isH1 屬性決定使用 <h1> 或 <h2> 標籤
+  const HeadingTag = isH1 ? "h1" : "h2";
+
   return (
     <div className={styles.heading_wrapper}>
       <img alt="標題左邊裝飾" className={styles.srcImage1} />
-      <h2 className={styles.heading} style={headingStyle}>
+      <HeadingTag className={styles.heading} style={headingStyle}>
         {headingContent}
-      </h2>
+      </HeadingTag>
       <img alt="標題右邊裝飾" className={styles.srcImage2} />
     </div>
   );

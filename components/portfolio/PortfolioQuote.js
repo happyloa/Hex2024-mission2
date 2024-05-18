53a67025f5448625844dd64abca0e0f279f9c83a
AaronHsieh <piafyoyo06@gmail.com>
2024-05-18 21:39:29 +0800

    修改其中一段引言成 h1 標籤。且為引言都加上 .quote class

--- components/portfolio/PortfolioQuote.js
+++ components/portfolio/PortfolioQuote.js
@@ -9,14 +9,14 @@ export default function PortfolioQuote() {
           alt="引言裝飾1"
           className={styles.quoteIcon1}
         />
-        <p>
+        <h1 className={styles.quote}>
           讓使用者在觀賞與使用的旅程中，發現
           <strong>
             設計的價值
             <img src="/image/deco/mark.webp" alt="標題裝飾" />
           </strong>
-        </p>
-        <p>
+        </h1>
+        <p className={styles.quote}>
           而我在設計與開發的過程中，看見
           <strong>
             自己的價值
